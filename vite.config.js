259e0b97a2e0da9948be7688ffb14d7efeae0cd0
mrcbq <marcobaquero@gmail.com>
2023-11-04 01:52:54 -0500

    change vite config out folder

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Carpeta de salida principal
+    assetsDir: 'assets', // Carpeta de activos (archivos estáticos)
+  },
 })
